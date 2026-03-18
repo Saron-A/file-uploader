a9d8ae206d3c51944b2f8eb77ef194dffa51a7f7
Saron <saronabebe2019@gmail.com>
2026-03-18 13:30:56 -0800

    Set up session-based authentication and bcrypt security

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: `${process.env.DB_URL}`,
   ssl: {
     rejectUnauthorized: false,
   },
--- script.js
+++ script.js
@@ -1,14 +1,58 @@
 require("dotenv").config();
-import express from "express";
-import passport from "passport";
-import session from "express-session";
-import cors from "cors";
+const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
+const localStrategy = require("passport-local").Strategy;
+const pool = require("./db/pool.js");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// step 1: initialize passport
 
+app.use(session({ secret: "", resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Step 2: define a strategy
+passport.use(
+  new localStrategy(async (username, password, done) => {
+    try {
+      const rows = await pool.query("SELECT * FROM User WHERE username = $1", [
+        username,
+      ]);
+      const user = rows[0];
+      if (!user) {
+        return done(null, false, { message: "User not found" });
+      }
+      // Here you would typically compare the provided password with the hashed password
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
+//Step 3: serialize and deserialize user
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const rows = await pool.query("SELECT * FROM User WHERE id = $1", [id]);
+    const user = rows[0];
+    return done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
 // routes
 
 // server start listening
